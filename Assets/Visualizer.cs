8a239eb80fb640059a4ab066ff850e0b5a1b3588
Takahiro Poly Horikawa <poly@curiosity-inc.jp>
2023-06-10 17:49:20 +0900

    AR FoundationのCamera Backgroundイメージに対しYoloを適用する

--- Assets/Visualizer.cs
+++ Assets/Visualizer.cs
@@ -2,16 +2,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using Klak.TestTools;
 using YoloV4Tiny;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Rendering;
 
 sealed class Visualizer : MonoBehaviour
 {
     #region Editable attributes
 
-    [SerializeField] ImageSource _source = null;
     [SerializeField, Range(0, 1)] float _threshold = 0.5f;
     [SerializeField] ResourceSet _resources = null;
     [SerializeField] RawImage _preview = null;
     [SerializeField] Marker _markerPrefab = null;
+    [SerializeField] ARCameraBackground m_ARCameraBackground = null;
+    [SerializeField] RenderTexture m_RenderTexture;
+
 
     #endregion
 
@@ -41,7 +45,9 @@ sealed class Visualizer : MonoBehaviour
 
     void Update()
     {
-        _detector.ProcessImage(_source.Texture, _threshold);
+        GetCameraImage();
+
+        _detector.ProcessImage(m_RenderTexture, _threshold);
 
         var i = 0;
         foreach (var d in _detector.Detections)
@@ -52,7 +58,46 @@ sealed class Visualizer : MonoBehaviour
 
         for (; i < _markers.Length; i++) _markers[i].Hide();
 
-        _preview.texture = _source.Texture;
+        _preview.texture = m_RenderTexture;
+    }
+
+    void GetCameraImage()
+    {
+        var commandBuffer = new CommandBuffer();
+        commandBuffer.name = "AR Camera Background Blit Pass";
+
+        // Get a reference to the AR Camera Background's main texture
+        // We will copy this texture into our chosen render texture
+        var texture = !m_ARCameraBackground.material.HasProperty("_MainTex") ?
+            null : m_ARCameraBackground.material.GetTexture("_MainTex");
+        Debug.Log($"texture is null?: {texture == null}");
+
+        // if (m_RenderTexture == null)
+        // {
+        //     m_RenderTexture = new RenderTexture()
+        // }
+
+        // Save references to the active render target before we overwrite it
+        var colorBuffer = Graphics.activeColorBuffer;
+        var depthBuffer = Graphics.activeDepthBuffer;
+
+        // Set Unity's render target to our render texture
+        Graphics.SetRenderTarget(m_RenderTexture);
+
+        // Clear the render target before we render new pixels into it
+        commandBuffer.ClearRenderTarget(true, false, Color.clear);
+
+        // Blit the AR Camera Background into the render target
+        commandBuffer.Blit(
+            texture,
+            BuiltinRenderTextureType.CurrentActive,
+            m_ARCameraBackground.material);
+
+        // Execute the command buffer
+        Graphics.ExecuteCommandBuffer(commandBuffer);
+
+        // Set Unity's render target back to its previous value
+        Graphics.SetRenderTarget(colorBuffer, depthBuffer);
     }
 
     #endregion
